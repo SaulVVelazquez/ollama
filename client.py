c131f3b558f9a66ebac52f83371112f0b0cc7354
SaulVVelazquez <1721110050@utectulancingo.edu.mx>
2024-05-27 21:44:13 +0000

    probando el simple generate

--- client.py
+++ client.py
@@ -1,47 +1,40 @@
 import json
 import requests
 
-# NOTE: ollama debe estar en funcionamiento para que esto funcione; inicia la aplicación o ejecuta `ollama serve`
-model = "llama3"  # TODO: actualiza esto con el nombre del modelo que desees usar
+# NOTA: ollama debe estar en funcionamiento para que esto funcione; inicia la aplicación o ejecuta `ollama serve`
+model = 'stablelm-zephyr'  # TODO: actualiza esto con el nombre del modelo que desees usar
 
-
-def chat(messages):
-    r = requests.post(
-        "http://0.0.0.0:11434/api/chat",
-        json={"model": model, "messages": messages, "stream": True},
-    )
+def generate(prompt, context):
+    r = requests.post('http://localhost:11434/api/generate',
+                      json={
+                          'model': model,
+                          'prompt': prompt,
+                          'context': context,
+                      },
+                      stream=True)
     r.raise_for_status()
-    output = ""
 
     for line in r.iter_lines():
         body = json.loads(line)
-        if "error" in body:
-            raise Exception(body["error"])
-        if body.get("done") is False:
-            message = body.get("message", "")
-            content = message.get("content", "")
-            output += content
-            # la respuesta se transmite un token a la vez, imprímelo a medida que lo recibimos
-            print(content, end="", flush=True)
+        response_part = body.get('response', '')
+        # la respuesta se transmite un token a la vez, imprímelo a medida que lo recibimos
+        print(response_part, end='', flush=True)
 
-        if body.get("done", False):
-            message["content"] = output
-            return message
+        if 'error' in body:
+            raise Exception(body['error'])
 
+        if body.get('done', False):
+            return body['context']
 
 def main():
-    messages = []
-
+    context = []  # el contexto almacena un historial de conversación, puedes usarlo para que el modelo sea más consciente del contexto
     while True:
         user_input = input("Ingresa una indicación: ")
         if not user_input:
             exit()
         print()
-        messages.append({"role": "user", "content": user_input})
-        message = chat(messages)
-        messages.append(message)
-        print("\n\n")
-
+        context = generate(user_input, context)
+        print()
 
 if __name__ == "__main__":
     main()
